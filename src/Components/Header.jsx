24330c43652415f245a44697e2b899c2e4d826bd
Alves <luciphoenix.soft@gmail.com>
2024-05-08 15:35:35 +0000

    Finished with header and its layout

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
     <>
-      <header className="px-3">
-        <div className="">
+      <header className="">
+        <div className="first-row p-3">
           <div className="row">
             <div className="col logo my-auto">Phoenix</div>
             <div className="col user-location d-flex align-items-center my-auto ">
@@ -12,15 +12,15 @@ function Header() {
                 <span>GHANA</span>
               </p>
             </div>
-            <form className="col-8 my-auto" action="#" method="get">
+            <form className="col-xl-8 col-sm-6 my-auto" action="#" method="get">
               <div className="input-group">
                 <button
-                  className="btn btn-outline-secondary dropdown-toggle"
+                  className="btn btn-outline-secondary bg-secondary text-white dropdown-toggle"
                   type="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  Dropdown
+                  Amazon Devices
                 </button>
                 <ul className="dropdown-menu">
                   <li>
@@ -53,7 +53,7 @@ function Header() {
                   aria-label="Text input with dropdown button"
                 />
                 <button className="input-group-text p-0" id="basic-addon2">
-                  <i className="bi bi-search"></i>
+                  <i className="bi bi-search mx-auto"></i>
                 </button>
               </div>
             </form>
@@ -88,13 +88,73 @@ function Header() {
                 </li>
               </ul>
             </div>
-            <a className="col my-auto" href="#">
-              Returns <br /> & Orders
+            <div className="col">
+              Hello,sign in <br />
+              <span>Account Lists</span>
+            </div>
+            <a className="col my-auto text-reset" href="#">
+              Returns <br />
+              <span>& Orders</span>
             </a>
-            <a className="col my-auto" href="#">
-              <i className="bi bi-cart4"></i>cart
+            <a className="col my-auto text-reset" href="#">
+              <i className="bi bi-cart4"></i>
+              <span>cart</span>
             </a>
           </div>
+        </div>
+        <div className="d-flex align-items-center second-row gap-5 px-3 py-2">
+          <a href="#" className="text-reset">
+            <i className="bi bi-list"></i>All
+          </a>
+          <a href="#" className="text-reset">
+            Today&apos;s Deal
+          </a>
+          <a href="#" className="text-reset">
+            Customer services
+          </a>
+          <a href="#" className="text-reset">
+            Registry
+          </a>
+          <a href="#" className="text-reset">
+            Gift cards
+          </a>
+          <a href="#" className="text-reset">
+            Sell
+          </a>
+        </div>
+        <div className="third-row d-flex">
+          <a href="#">Amazon Devices</a>
+          <a href="#">Device Deals</a>
+          <a href="#">
+            Echo &amp; Alexa <i className="bi bi-caret-down-fill"></i>
+          </a>
+          <a href="#">
+            Fire Tablets <i className="bi bi-caret-down-fill"></i>
+          </a>
+          <a href="#">
+            Fire TV <i className="bi bi-caret-down-fill"></i>
+          </a>
+          <a href="#">
+            Kindle <i className="bi bi-caret-down-fill"></i>
+          </a>
+          <a href="#">
+            Connected Home <i className="bi bi-caret-down-fill"></i>
+          </a>
+          <a href="#">
+            Luna Gaming <i className="bi bi-caret-down-fill"></i>
+          </a>
+          <a href="#">
+            Day 1 Editions <i className="bi bi-caret-down-fill"></i>
+          </a>
+          <a href="#">
+            Accessories <i className="bi bi-caret-down-fill"></i>
+          </a>
+          <a href="#">
+            Pre-owned Devices <i className="bi bi-caret-down-fill"></i>
+          </a>
+          <a href="#">
+            Trade-In <i className="bi bi-caret-down-fill"></i>
+          </a>
         </div>
       </header>
     </>
