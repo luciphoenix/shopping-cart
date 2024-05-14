b80cafeb3e7ed79dd08bf40737b0f5c0c9423418
Alves <luciphoenix.soft@gmail.com>
2024-05-14 19:37:16 +0000

    edited the aside

--- src/Components/Aside.jsx
+++ src/Components/Aside.jsx
@@ -1,5 +1,63 @@
 function Aside() {
-  return <aside>Aside</aside>;
+  return (
+    <aside>
+      <div className="aside-container">
+        <h3 className="text-center">FILTER</h3>
+        <div className="filter-list">
+          <div>
+            <a href="#">
+              <i className="bi bi-shop"></i>Supermarket
+            </a>
+          </div>
+          <div>
+            <a href="#">
+              <i className="bi bi-phone"></i>Phones &amp; Tablets
+            </a>
+          </div>
+          <div>
+            <a href="#">
+              <i className="bi bi-hospital"></i>Health &amp; Beauty
+            </a>
+          </div>
+          <div>
+            <a href="#">
+              <i className="bi bi-house"></i>Home &amp; Office
+            </a>
+          </div>
+          <div>
+            <a href="#">
+              <i className="bi bi-device-ssd"></i>Appliances
+            </a>
+          </div>
+          <div>
+            <a href="#">
+              <i className="bi bi-ev-front"></i>Electronics
+            </a>
+          </div>
+          <div>
+            <a href="#">Computing</a>
+          </div>
+          <div>
+            <a href="#">Fashion</a>
+          </div>
+          <div>
+            <a href="#">Sporting Goods</a>
+          </div>
+          <div>
+            <a href="#">Baby Products</a>
+          </div>
+          <div>
+            <a href="#">
+              <i className="bi bi-controller"></i>Gaming
+            </a>
+          </div>
+          <div>
+            <a href="#">Other categories</a>
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
 }
 
 export default Aside;
