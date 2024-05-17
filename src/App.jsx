100b41bb5bd605c5044459fa396327638b0965c2
Alves <luciphoenix.soft@gmail.com>
2024-05-17 17:39:26 +0000

    added footer buttons

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,6 @@ function App() {
         <Aside />
         <Main />
       </div>
-
       <Footer />
     </>
   );
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
       <a href="#amazon" className="back-to-top fs-4 d-block text-center">
         Back to Top
       </a>
-      <div className="d-flex justify-content-center gap-5 py-5">
+      <div className="footer-links d-flex justify-content-center gap-5 py-5">
         <div>
           <h4>Get to Know Us</h4>
           <a href="#" className="d-block">
@@ -93,6 +93,22 @@ function Footer() {
           </a>
         </div>
       </div>
+      <hr />
+      <div className="footer-button d-flex align-text-center justify-content-center py-4">
+        <div className="logo">phoenix</div>
+        <a href="#" className="link-btn">
+          <i className="bi bi-globe2"></i> English
+          <i className="bi bi-caret-down-fill"></i>
+        </a>
+        <a href="#" className="link-btn">
+          <i className="bi bi-currency-dollar"></i>
+          USD - U.S. Dollar
+        </a>
+        <a href="#" className="link-btn">
+          <img src="/image.png" alt="" /> United State
+        </a>
+      </div>
+      <div className="footer-items py-2 ">what</div>
     </footer>
   );
 }
